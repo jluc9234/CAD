c42edeffb77615ff6ad206d8846d80c891e60b56
jluc9234 <dogpoundreject1@gmail.com>
2025-10-01 17:21:22 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,1 @@
-import path from 'path';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-// This is the correct, secure configuration.
-export default defineConfig({
-  base: "/",
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
-    }
-  }
-});
